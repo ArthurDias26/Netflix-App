05dd64b505acb0ef6571a22b9c8d3a4a7ce9c8ca
Your Name <you@example.com>
2022-08-04 14:48:00 -0300

    responsive

--- src/components/layout/MovieBlock.js
+++ src/components/layout/MovieBlock.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from 'react-router-dom'
 import styles from './styles/MovieBlock.module.css'
+import Loading from "./Loading"
 
 export default function MovieBlock({movieData, title}){
 
@@ -8,7 +9,7 @@ export default function MovieBlock({movieData, title}){
 
     return(
         <>
-            {movieData ? (
+            {movieData && (
                 <div className={styles.movieBlock_container}>
 
                     <h1 className={styles.movieBlock_title}>{title}</h1>
@@ -16,25 +17,27 @@ export default function MovieBlock({movieData, title}){
                     <div className={styles.red_line}></div>
 
                     <div className={styles.movie_items_container}>
-                        {movieData.map((movie, index) =>(
+                        {movieData.length >= 1 ? (
                             <>
-                                {movie.poster_path && (
-                                    <div className={styles.movie_item} key={index}>
-                                        <Link to={`/exhibition/${movie.name ? 'series' : 'movies'}${movie.id}`}>
-                                            <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.name ? movie.name : movie.title}/>
-                                        </Link>
-                                    </div>
-                                )}
+                                {movieData.map((movie, index) =>(
+                                    <>
+                                        {movie.poster_path && (
+                                            <div className={styles.movie_item} key={index}>
+                                                <Link to={`/exhibition/${movie.name ? 'series' : 'movies'}${movie.id}`}>
+                                                    <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.name ? movie.name : movie.title}/>
+                                                </Link>
+                                            </div>
+                                        )}
+                                    </>
+                                ))}
                             </>
-                            
-                        ))}
+                        ) : (
+                        <p>no items found, make sure the search field is correct</p>
+                        )}
+                        
                     </div>
 
                 </div> 
-            ):
-
-            (
-                <h1>Não foi</h1>
             )}
         </>
     )
