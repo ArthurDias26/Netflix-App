6ebe89b9734016184b8ba6a69d9d43c7873ad51e
Your Name <you@example.com>
2022-08-09 11:07:07 -0300

    request

--- src/request.js
+++ src/request.js
@@ -27,49 +27,49 @@ export default {
             {
                 slug: 'originals',
                 title: 'Netflix Originals',
-                items: await simpleFetch(`discover/tv/?with_network=123&api_key=${API_key}`)
+                items: await simpleFetch(`discover/tv?with_network=123&api_key=${API_key}`)
             },
             
             {
                 slug: 'trending',
                 title: 'For you',
-                items: await simpleFetch(`trending/all/week/?api_key=${API_key}`) 
+                items: await simpleFetch(`trending/all/week?api_key=${API_key}`) 
             },
     
             {
                 slug: 'toptread',
                 title: 'Top tread',
-                items: await simpleFetch(`movie/top_rated/?api_key=${API_key}`) 
+                items: await simpleFetch(`movie/top_rated?api_key=${API_key}`) 
             },
     
             {
                 slug: 'action',
                 title: 'Top Treanding TV',
-                items: await simpleFetch(`trending/tv/week/?api_key=${API_key}`) // trending/movie/week?api_key=
+                items: await simpleFetch(`trending/tv/week?api_key=${API_key}`) // trending/movie/week?api_key=
             },
     
             {
                 slug: 'comedy',
                 title: 'Top Treanding Movies',
-                items: await simpleFetch(`trending/movie/week/?api_key=${API_key}`) 
+                items: await simpleFetch(`trending/movie/week?api_key=${API_key}`) 
             },
     
             {
                 slug: 'horror',
                 title: 'Horror',
-                items: await simpleFetch(`trending/tv/day/?api_key=${API_key}`) 
+                items: await simpleFetch(`trending/tv/day?api_key=${API_key}`) 
             },
     
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await simpleFetch(`trending/movie/day/?api_key=${API_key}`) 
+                items: await simpleFetch(`trending/movie/day?api_key=${API_key}`) 
             },
     
             {
                 slug: 'documentary',
                 title: 'Docomentary',
-                items: await simpleFetch(`discover/movie/?with_geners=99&api_key=${API_key}`) 
+                items: await simpleFetch(`discover/movie?with_geners=99&api_key=${API_key}`) 
             }
         ]
     } 
@@ -80,16 +80,17 @@ export default {
 
 export async function RequestMovie(type, id) {
     if (id && type) {
-    return await simpleFetch(`${type}/${id}/?api_key=${API_key}&append_to_response=videos`)
+    return await simpleFetch(`${type}/${id}?api_key=${API_key}&append_to_response=videos`)
     }
 }
 
 export async function RequestMovieSimilar(type, id) {
     if (id && type) {
-    return await simpleFetch(`${type}/${id}/similar/?api_key=${API_key}&append_to_response=videos`)
+    return await simpleFetch(`${type}/${id}/similar?api_key=${API_key}&append_to_response=videos`)
     }
 }
+//https://api.themoviedb.org/3/search/multi?api_key=b36c2057f9e810360f8e3a13acc519d5&query=Jura
 
 export async function RequestMovieSearch(query) {
-     return await simpleFetch(`search/multi/?api_key=${API_key}&query=${query}`)
+     return await simpleFetch(`search/multi?api_key=${API_key}&query=${query}`)
 }
